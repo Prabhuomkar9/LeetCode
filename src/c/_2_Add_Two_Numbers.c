dadf9663920fab8eafe78ebb11f3eafb23d91511
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-09-12 22:07:00 +0530

    Created _2_Add_Two_Numbers.c

--- src/c/_2_Add_Two_Numbers.c
+++ src/c/_2_Add_Two_Numbers.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct ListNode
-{
-    int val;
-    struct ListNode *next;
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
-    struct ListNode *dummy;
-    dummy->val = 0;
-    dummy->next = NULL;
-    struct ListNode *curr = dummy;
-    int carry = 0;
-
-    while (l1 != NULL || l2 != NULL || carry == 1)
+    struct ListNode *first = (struct ListNode *)malloc(sizeof(struct ListNode));
+    first->val = 0;
+    first->next = NULL;
+    struct ListNode *point = first;
+    int sum = 0, carry = 0;
+    while (l1 != NULL || l2 != NULL || carry != 0)
     {
-        int sum = 0;
         if (l1 != NULL)
         {
             sum += l1->val;
@@ -30,27 +26,12 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
         }
         sum += carry;
         carry = sum / 10;
-        struct ListNode *temp = (struct Listnode *)malloc(sizeof(struct ListNode));
-        temp->val = sum % 10;
-        temp->next = NULL;
-        curr->next = temp;
-        curr = curr->next;
+        struct ListNode *newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+        point->next = newNode;
+        point = point->next;
+        sum = 0;
     }
-}
-
-int main(int argc, char const *argv[])
-{
-    struct ListNode l13, l12, l1, l23, l22, l2;
-    l23.val = 4;
-    l22.val = 6;
-    l22.next = &l23;
-    l2.val = 5;
-    l2.next = &l22;
-    l13.val = 3;
-    l12.val = 4;
-    l12.next = &l13;
-    l1.val = 2;
-    l1.next = &l12;
-    struct ListNode *ans = addTwoNumbers(&l1, &l2);
-    return 0;
+    return first->next;
 }
