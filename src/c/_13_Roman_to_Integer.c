e9e2547230cc28ecefd240e16d903c5aad2b29dc
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-09-16 20:56:03 +0530

    update

--- src/c/_13_Roman_to_Integer.c
+++ src/c/_13_Roman_to_Integer.c
@@ -1,29 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
-#include <stdbool.h>
-
 int romanToInt(char *s)
 {
-    int t['X' + 1] = {['I'] = 1, ['V'] = 5, ['X'] = 10, ['L'] = 50, ['C'] = 100, ['D'] = 500, ['M'] = 1000};
+    int map['X' + 1] = {0};
+    map['I'] = 1, map['V'] = 5, map['X'] = 10, map['L'] = 50, map['C'] = 100, map['D'] = 500, map['M'] = 1000;
     int res = 0;
     for (int i = 0; s[i]; i++)
     {
-        if (t[s[i]] < t[s[i + 1]])
-            res -= t[s[i]];
+        if (map[s[i]] < map[s[i + 1]])
+            res -= map[s[i]];
         else
-            res += t[s[i]];
+            res += map[s[i]];
     }
     return res;
-}
-
-// Driver code
-int main(int argc, char const *argv[])
-{
-    char str[] = "MMMDCCCLXXXVIII";
-    printf("%d", romanToInt(str));
-    return 0;
 }
