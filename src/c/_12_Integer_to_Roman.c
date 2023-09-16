409478f44e91c9130acca5796e580141e170abe3
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-09-16 19:28:46 +0530

    created 12

---
+++ src/c/_12_Integer_to_Roman.c
@@ -0,0 +1,15 @@
+char *intToRoman(int num)
+{
+    int size;
+    char *M[] = {"", "M", "MM", "MMM"};
+    char *C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    char *X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    char *I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    size = strlen(M[num / 1000]) + strlen(C[(num % 1000) / 100]) + strlen(X[(num % 100) / 10]) + strlen(I[num % 10]) + 1;
+    char *ans = malloc(size);
+    strcpy(ans, M[num / 1000]);
+    strcat(ans, C[(num % 1000) / 100]);
+    strcat(ans, X[(num % 100) / 10]);
+    strcat(ans, I[num % 10]);
+    return ans;
+}
