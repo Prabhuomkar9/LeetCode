d3dae14d8a028fb6b9330ab31a821fa8e2b4155a
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-13 12:50:12 +0530

    update

---
+++ src/java/_0746_Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+
+        dp[cost.length - 1] = cost[cost.length - 1];
+        dp[cost.length - 2] = cost[cost.length - 2];
+
+        for (int i = cost.length - 3; i >= 0; i--) {
+            if (dp[i + 1] < dp[i + 2])
+                dp[i] = cost[i] + dp[i + 1];
+            else
+                dp[i] = cost[i] + dp[i + 2];
+        }
+
+        return dp[0] < dp[1] ? dp[0] : dp[1];
+    }
+}
