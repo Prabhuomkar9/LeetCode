769577be2efd87e209cfb83b9aa53a084630a32d
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-24 16:57:04 +0530

    update

---
+++ src/java/_1071_Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+
+        return str1.substring(0, this.gcd(str1.length(), str2.length()));
+    }
+}
