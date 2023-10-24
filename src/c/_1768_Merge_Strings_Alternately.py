a68c55aa9c18bc92ac587188c94ad0f07f9d6bb3
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-24 14:00:33 +0530

    update

---
+++ src/c/_1768_Merge_Strings_Alternately.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0
+        res = ""
+
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                res += word1[i]
+            if i < len(word2):
+                res += word2[i]
+            i += 1
+
+        return res
