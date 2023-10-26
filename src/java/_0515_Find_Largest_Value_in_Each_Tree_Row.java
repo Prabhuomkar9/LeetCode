ff78b28745a78e4f90104af7df9ef62e0144cac3
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-26 22:03:34 +0530

    update

--- src/java/_0515_Find_Largest_Value_in_Each_Tree_Row.java
+++ src/java/_0515_Find_Largest_Value_in_Each_Tree_Row.java
@@ -1,26 +1,26 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     private List<Integer> ans = new ArrayList<>();
 
-    private void dfs(TreeNode root, int row){
-        if(root == null)
+    private void dfs(TreeNode root, int row) {
+        if (root == null)
             return;
 
-        if(this.ans.size() <= row)
+        if (this.ans.size() <= row)
             ans.add(root.val);
         else
             ans.set(row, Math.max(ans.get(row), root.val));
