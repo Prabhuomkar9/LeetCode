05603ecbc81a79bd4f8a84a989cd75702c5aaa47
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-27 21:40:03 +0530

    update

--- src/python/_1268_Search_Suggestions_System.py
+++ src/python/_1268_Search_Suggestions_System.py
@@ -1,3 +1,30 @@
+class Trie:
+    def __init__(self, li):
+        self.trie = {"#": []}
+        for ele in li:
+            self.insert(ele)
+
+    def insert(self, word):
+        curr = self.trie
+
+        for c in word:
+            if c not in curr:
+                curr[c] = {"#": []}
+            if len(curr[c]["#"]) < 3:
+                curr[c]["#"].append(word)
+            curr = curr[c]
+
+    def search(self, prefix):
+        curr = self.trie
+
+        for c in prefix:
+            if c not in curr:
+                return []
+            curr = curr[c]
+
+        return curr["#"]
+
+
 class Solution:
     def suggestedProducts(
         self, products: List[str], searchWord: str
@@ -5,15 +32,10 @@ class Solution:
         ans = []
         products.sort()
 
+        t = Trie(products)
+
         for i in range(len(searchWord)):
             search = searchWord[0 : i + 1]
-            l = []
-            for product in products:
-                if len(l) == 3:
-                    break
-
-                if product.startswith(search):
-                    l.append(product)
-            ans.append(l)
+            ans.append(t.search(search))
 
         return ans
