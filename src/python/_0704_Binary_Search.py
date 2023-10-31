b4d02187b567e3b89489ecbf79b6efcad4f0f1a4
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-10-31 20:01:50 +0530

    update

--- src/python/_0704_Binary_Search.py
+++ src/python/_0704_Binary_Search.py
@@ -1,15 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, h = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
 
-        while l <= h:
-            m = (l + h) // 2
+        while l <= r:
+            m = l + (r - l) // 2
 
-            if nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
-                h = m - 1
-            else:
+            if nums[m] == target:
                 return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
 
         return -1
