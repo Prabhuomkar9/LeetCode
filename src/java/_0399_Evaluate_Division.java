9027d89c627b052a49ac4337b9d8a78a1dda40ad
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-11-05 14:23:34 +0530

    update

--- src/java/_0399_Evaluate_Division.java
+++ src/java/_0399_Evaluate_Division.java
@@ -1,20 +1,59 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
-    double dfs(String u, String v, HashSet<String> seen, HashMap<String, HashMap<String, Double>> graph) {
-        return -1;
-    }
+    private double dfs(String u, String v, Map<String, Map<String, Double>> graph, Set<String> seen) {
+        if (graph.get(u).containsKey(v))
+            return graph.get(u).get(v);
 
-    List<Double> calcEquation(List<List<String>> equations, List<Double> values, List<List<String>> queries) {
-        List<Double> ans = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : graph.get(u).entrySet()) {
+            String key = entry.getKey();
+            double value = entry.getValue();
 
-        for(List<String> : queries){
+            if (seen.contains(key))
+                continue;
 
+            seen.add(u);
+            double val = dfs(key, v, graph, seen);
+            seen.remove(u);
+            if (val > 0)
+                return value * val;
         }
 
-        return ans;
+        return -1.0;
+    }
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+
+        for (int i = 0; i < values.length; i++) {
+            String u = equations.get(i).get(0), v = equations.get(i).get(1);
+
+            if (!graph.containsKey(u))
+                graph.put(u, new HashMap<>());
+
+            if (!graph.containsKey(v))
+                graph.put(v, new HashMap<>());
+
+            graph.get(u).put(v, values[i]);
+            graph.get(v).put(u, 1 / values[i]);
+        }
+
+        List<Double> ans = new ArrayList<>();
+
+        for (int i = 0; i < queries.size(); i++) {
+            String u = queries.get(i).get(0), v = queries.get(i).get(1);
+
+            if (!graph.containsKey(u) || !graph.containsKey(v)) {
+                ans.add(-1.0);
+            } else {
+                Set<String> seen = new HashSet<>();
+                ans.add(dfs(u, v, graph, seen));
+            }
+        }
+
+        double[] resultArray = new double[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++)
+            resultArray[i] = ans.get(i);
+
+        return resultArray;
     }
 }
