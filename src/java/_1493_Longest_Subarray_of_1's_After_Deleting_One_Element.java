57681e2aa0618c2744580bf3ce51ba366e1686b6
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-11-07 21:15:58 +0530

    update

--- src/java/_1493_Longest_Subarray_of_1's_After_Deleting_One_Element.java
+++ src/java/_1493_Longest_Subarray_of_1's_After_Deleting_One_Element.java
@@ -2,10 +2,10 @@ class Solution {
     public int longestSubarray(int[] nums) {
         int l = 0, r = 0, zeroIndex = -1, ans = 0;
 
-        while(r < nums.length){
-            if(nums[r] == 0){
+        while (r < nums.length) {
+            if (nums[r] == 0) {
                 ans = Math.max(ans, r - l - 1);
-                if(zeroIndex >= 0)
+                if (zeroIndex >= 0)
                     l = zeroIndex + 1;
                 zeroIndex = r;
             }
@@ -14,7 +14,7 @@ class Solution {
 
         ans = Math.max(ans, r - l - 1);
 
-        if zeroIndex < 0:
+        if (zeroIndex < 0)
             return nums.length - 1;
 
         return ans;
