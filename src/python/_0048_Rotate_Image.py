3498ba0f39d0e3e2d734311e94f6b18add439896
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-12-15 19:37:08 +0530

    update

--- src/python/_0048_Rotate_Image.py
+++ src/python/_0048_Rotate_Image.py
@@ -4,6 +4,6 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        for i in range(0, n // 2):
-            for j in range(i, n - i - 1):
-                matrix[j][n - i - 1], matrix[n - i - 1][n - j - 1], matrix[n - j - 1][i], matrix[i][j] = matrix[i][j], matrix[j][n - i - 1], matrix[n - i - 1][n - j - 1], matrix[n - j - 1][i]
+        for i in range((n + 1) // 2):
+            for j in range(n // 2):
+                matrix[i][j], matrix[j][~i], matrix[~i][~j], matrix[~j][i] = matrix[~j][i], matrix[i][j], matrix[j][~i], matrix[~i][~j]
