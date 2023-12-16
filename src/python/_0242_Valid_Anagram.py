854cd0803c87d6c62e1ce52ae907c046a7051abf
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-12-16 08:46:56 +0530

    update

--- src/python/_0242_Valid_Anagram.py
+++ src/python/_0242_Valid_Anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        seen = {}
-
-        for c in s:
-            seen[c] = seen[c] + 1 if c in seen else 1
-
-        for c in t:
-            if c not in seen:
-                return False
-
-            if seen[c] == 0:
-                return False
-            seen[c] -= 1
-
-        return True
+        return Counter(s) == Counter(t)
