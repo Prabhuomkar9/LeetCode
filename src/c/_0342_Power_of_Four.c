5ea3c253f61a600c1fd8465302de2adf79d256bd
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-10-25 18:56:27 +0530

    [refac] cpp

--- src/c/_0342_Power_of_Four.c
+++
@@ -1,4 +0,0 @@
-bool isPowerOfFour(int n)
-{
-    return n > 0 && (n & (n - 1)) == 0 && (n | 0xAAAAAAAA) != 0xAAAAAAAA;
-}
--- src/python/_1980_Find_Unique_Binary_String.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDifferentBinaryString(self, nums: List[str]) -> str:
-        ans = ""
-
-        for i, num in enumerate(nums):
-            ans += "1" if num[i] == "0" else "0"
-
-        return ans
