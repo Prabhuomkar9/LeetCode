66c7f7bbd640ea98bc697a8baf53139c3106c440
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-10-25 19:00:13 +0530

    [refac] bash

--- src/python/_0058_Length_of_Last_Word.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        s = s.split(" ")
-        return len(s[-1])
--- src/python/_0136_Single_Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-
-        for num in nums:
-            ans ^= num
-
-        return ans
